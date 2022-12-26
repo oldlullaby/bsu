a42339e801d3ade089fc88472c76cfee0d3aa1db
Maksim Parfianienka <115582642+oldlullaby@users.noreply.github.com>
2022-12-26 12:17:10 +0300

    Create MainTest.java

---
+++ Industrial Programming/Lab_7/JUnit_for_Lab1/src/MainTest.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class MainTest {
+
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
+
+    private ByteArrayInputStream testIn;
+    private ByteArrayOutputStream testOut;
+
+    @Before
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    private void provideInput(String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    private String getOutput() {
+        return testOut.toString();
+    }
+
+    @After
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+
+    @Test
+    public void testCase1() {
+        final String testString_1 = "4 2"; provideInput(testString_1);
+        Main.main(new String[1]);
+        assertEquals("Enter k & x:\n" + "Our result: 0,90929\n" +
+                "\n" +
+                "Result using sin function: 0,90930\n" +
+                "\n", getOutput());
+    }
+
+
+}
